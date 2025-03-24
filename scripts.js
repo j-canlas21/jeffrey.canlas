f9a1b708774093c71c5bcc56541e196be96076f2
j-canlas21 <j.canlas897@gmail.com>
2025-03-24 17:52:34 +0800

    Create scripts.js
    all scripts

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Mobile menu toggle
+document.querySelector('.logo').addEventListener('click', () => {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});
